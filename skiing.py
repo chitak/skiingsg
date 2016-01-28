a5289be8711ae018ae49a3caec3c2b682d1785ef
Lam Chi Tak <ctak225@gmail.com>
2016-01-28 22:14:46 +0800

    code clean up

--- skiing.py
+++ skiing.py
@@ -1,5 +1,6 @@
-from random import randint, shuffle
+import time
 
+from random import randint, shuffle
 
 class Matrix:
 
@@ -18,35 +19,45 @@ class Matrix:
                 inner.append(int(splited.replace('\n', '')))
             self.matrix.append(inner)
 
-    def get(self, x, y):
-        return self.matrix[x][y]
+    def get(self, coord):
+        return self.matrix[coord[0]][coord[1]]
 
-    def up(self, x, y):
+    def up(self, coord):
+        x = coord[0]
+        y = coord[1]
+
         if (y-1) < 0:
             return None
         return (x, y-1)
 
-    def down(self, x, y):
+    def down(self, coord):
+        x = coord[0]
+        y = coord[1]
+
         if (y+1) >= self.row:
             return None
         return (x, y+1)
 
-    def left(self, x, y):
+    def left(self, coord):
+        x = coord[0]
+        y = coord[1]
+
         if (x-1) < 0:
             return None
         return (x-1, y)
 
-    def right(self, x, y):
+    def right(self, coord):
+        x = coord[0]
+        y = coord[1]
+
         if (x+1) >= self.row:
             return None
         return (x+1, y)
 
 
-
 def dfs_path(matrix, start, path=[], longest=None):
     path = path + [start]
-    x, y = start
-    val = matrix.get(x, y)
+    val = matrix.get(start)
 
     # print "current path: ", path
     # print "longest: ", longest
@@ -54,54 +65,41 @@ def dfs_path(matrix, start, path=[], longest=None):
     childs = []
 
     up_val = None
-    up = matrix.up(x, y)
+    up = matrix.up(start)
     if up is not None:
         up_val = matrix.get(up)
     if up_val is not None and up_val < val:
         childs.append(up)
 
     down_val = None
-    down = matrix.down(x, y)
+    down = matrix.down(start)
     if down is not None:
         down_val = matrix.get(down)
     if down_val is not None and down_val < val:
         childs.append(down)
 
     left_val = None
-    left = matrix.left(x, y)
+    left = matrix.left(start)
     if left is not None:
         left_val = matrix.get(left)
     if left_val is not None and left_val < val:
         childs.append(left)
 
     right_val = None
-    right = matrix.right(x, y)
+    right = matrix.right(start)
     if right is not None:
         right_val = matrix.get(right)
     if right_val is not None and right_val < val:
         childs.append(right)
 
     if not childs:
-        new_val = []
-        for node in path:
-            new_val.append(matrix.get(node[0], node[1]))
-
-        longest_val = []
-        for node in longest:
-            longest_val.append(matrix.get(node[0], node[1]))
-
-        diff_path = 0
-        if new_val:
-            diff_path = max(new_val) - min(new_val)
-
-        diff_longest = 0
-        if longest_val:
-            diff_longest = max(longest_val) - min(longest_val)
+        drop_path = drop(path)
+        drop_longest = drop(longest)
 
         if len(path) > len(longest):
             longest = path
         elif len(path) == len(longest):
-            if diff_path >= diff_longest:
+            if drop_path >= drop_longest:
                 longest = path
         return longest
 
@@ -109,30 +107,24 @@ def dfs_path(matrix, start, path=[], longest=None):
         if child not in path:
             new_path = dfs_path(matrix, child, path, longest)
 
-            new_val = []
-            for node in new_path:
-                new_val.append(matrix.get(node[0], node[1]))
-
-            longest_val = []
-            for node in longest:
-                longest_val.append(matrix.get(node[0], node[1]))
-
-            diff_newpath = 0
-            if new_val:
-                diff_newpath = max(new_val) - min(new_val)
-
-            diff_longest = 0
-            if longest_val:
-                diff_longest = max(longest_val) - min(longest_val)
+            drop_newpath = drop(new_path)
+            drop_longest = drop(longest)
 
             if new_path is not None:
                 if len(new_path) > len(longest):
                     longest = new_path
                 elif len(new_path) == len(longest):
-                    if diff_newpath >= diff_longest:
+                    if drop_newpath >= drop_longest:
                         longest = new_path
 
     return longest
+
+
+def drop(path):
+    vals = [matrix.get(node) for node in path]
+    if vals:
+        return max(vals) - min(vals)
+    return 0
 
 
 def gen_matrix(m):
@@ -160,11 +152,14 @@ if __name__ == '__main__':
     matrix = Matrix("map.txt")
 
     longest = []
+    start = time.time()
     for i in range(matrix.row):
         for j in range(matrix.row):
             longest = dfs_path(matrix, (i, j), [], longest)
+            # print i, j, longest
+    end = time.time()
 
-            print i, j, longest
-
+    print "used: {0}(s)".format((end - start))
+    print "length: {0}, drop: {1}".format(len(longest), drop(longest))
     for node in longest:
-        print matrix.get(node[0], node[1]),
+        print matrix.get(node),
